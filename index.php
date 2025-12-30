6853443f4b43dbdbfd931c40c610241f1d8d31b2
Osama Hamedah <220372646+OsamaHamedah@users.noreply.github.com>
2025-12-30 18:15:54 +0300

    Add database connection configuration and update index

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php
 //Personal note: This is the presentation layer".
+include 'config/db_connection.php';
+
 echo "<h1>Cyber Lens Project</h1>";
 
-?>
+if(isset($conn) && $conn->ping()) {
+    echo "<h1>Connection is established successfully. </h1>";
+    //echo "<p>Connected to database : " . $dbname . "</p>";
+    //It kept showing static / IDE warning (made it a comment in order to avoid future errors)
+}
+
+else {
+    echo "<h1>Connection is not established. </h1>";
+}
